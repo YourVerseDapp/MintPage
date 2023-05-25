fb5d9516fc76e5fccf742735ea37ec10e7926896
xDiamondsx <115242699+xDiamondsx@users.noreply.github.com>
2023-05-25 19:31:42 +0200

    upload

--- const/yourDetails.ts
+++ const/yourDetails.ts
@@ -1,5 +1,5 @@
 // Replace this with your contract address
-export const myEditionDropContractAddress: string = "0xDC8017E1E20BFF80a49B0B92F719f00170013B4F";
+export const myEditionDropContractAddress: string = "0x753005C9481Bd37bc371539D665c929B104206Ec";
 
 // Replace this with your token id
 export const tokenId: string = "0";
